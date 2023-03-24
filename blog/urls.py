d53362bb2393525389c490ad52a46f537a999e99
pneumA1 <mikhalovskiys@gmail.com>
2023-03-25 01:48:59 +0300

    added views blog and single

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog.views import *
+
+app_name = 'blog'
+
+urlpatterns =[
+    path('blog/', blog, name='blog'),
+    path('single/', single, name='single'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blog(request):
+    return render(request, 'blog/blog.html')
+
+def single(request):
+    return render(request, 'blog/single.html')
