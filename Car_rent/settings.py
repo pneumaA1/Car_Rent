f5f72b0a2964f4824da0a68ca55386324c96f452
pneumA1 <mikhalovskiys@gmail.com>
2023-03-30 22:50:34 +0300

    updated settings file

--- Car_rent/settings.py
+++ Car_rent/settings.py
@@ -1,7 +1,8 @@
 import os
-
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3r4+6au!ne6=e)z*c0z&+rsx-$+^op6vz27qn4u$f7fvx#zvh'
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
